9523a478d9a2f41186c815e366fd7c5e45221318
Gevork-Sarkisyan222 <zadroterkom@gmail.com>
2024-01-14 19:47:10 +0400

    adding api into browser

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5555',
+  baseURL: 'https://chat-server-f146.onrender.com',
 });
 
 instance.interceptors.request.use((config) => {
